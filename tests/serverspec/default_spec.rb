bb5f29f3cdb7e7d13c0a4c537dba59b3f4bb889e
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-17 16:10:48 +0900

    rubocopfy

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -18,26 +18,30 @@ end
 
 describe file("/usr/local/etc/pkg/repos/FreeBSD.conf") do
   it { should be_file }
-  its(:content_as_yaml) { should include(
-    "FreeBSD" => include(
-      "url" => "pkg+http://pkg.FreeBSD.org/${ABI}/latest",
-      "mirror_type" => "srv",
-      "signature_type" => "fingerprints",
-      "fingerprints" => "/usr/share/keys/pkg",
-      "enabled" => false
+  its(:content_as_yaml) do
+    should include(
+      "FreeBSD" => include(
+        "url" => "pkg+http://pkg.FreeBSD.org/${ABI}/latest",
+        "mirror_type" => "srv",
+        "signature_type" => "fingerprints",
+        "fingerprints" => "/usr/share/keys/pkg",
+        "enabled" => false
+      )
     )
-  )}
+  end
 end
 
 describe file("/usr/local/etc/pkg/repos/Reallyenglish.conf") do
   it { should be_file }
-  its(:content_as_yaml) { should include(
-    "Reallyenglish" => include(
-      "url" => "http://10.3.build.reallyenglish.com/${ABI}",
-      "mirror_type" => "http",
-      "signature_type" => "none",
-      "fingerprints" => "NONE",
-      "enabled" => true
+  its(:content_as_yaml) do
+    should include(
+      "Reallyenglish" => include(
+        "url" => "http://10.3.build.reallyenglish.com/${ABI}",
+        "mirror_type" => "http",
+        "signature_type" => "none",
+        "fingerprints" => "NONE",
+        "enabled" => true
+      )
     )
-  )}
+  end
 end
