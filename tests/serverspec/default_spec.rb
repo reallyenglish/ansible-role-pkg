f2f1752cfc66ab04ff312a980f67e289d454ee81
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-17 16:09:51 +0700

    [feature][bugfix]  support all possible configurations in pkg.conf(5) (#6)
    * QA
    
    * support all posiible configurations in pkg.conf(5)
    
    * refactor tests
    
    * fix wrong variable name in README
    
    * rubocopfy
    
    * failout when ansible_os_family is not FreeBSD
    
    also
    
    * fix a typo
    
    https://github.com/reallyenglish/ansible-role-pkg/pull/6#pullrequestreview-69796748

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -10,21 +10,38 @@ end
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape("syslog: true") }
+  its(:content_as_yaml) { should include("SYSLOG" => true) }
+  its(:content_as_yaml) { should include("FETCH_TIMEOUT" => 300) }
+  its(:content_as_yaml) { should include("FETCH_RETRY" => 5) }
+  its(:content_as_yaml) { should include("REPOS_DIR" => ["/etc/pkg/", "/usr/local/etc/pkg/repos/"]) }
 end
 
 describe file("/usr/local/etc/pkg/repos/FreeBSD.conf") do
   it { should be_file }
-  its(:content) { should match(/FreeBSD: {/) }
-  its(:content) { should match(/enabled: no/) }
+  its(:content_as_yaml) do
+    should include(
+      "FreeBSD" => include(
+        "url" => "pkg+http://pkg.FreeBSD.org/${ABI}/latest",
+        "mirror_type" => "srv",
+        "signature_type" => "fingerprints",
+        "fingerprints" => "/usr/share/keys/pkg",
+        "enabled" => false
+      )
+    )
+  end
 end
 
 describe file("/usr/local/etc/pkg/repos/Reallyenglish.conf") do
   it { should be_file }
-  its(:content) { should match(/Reallyenglish: {/) }
-  its(:content) { should match Regexp.escape('url: "http://10.3.build.reallyenglish.com/${ABI}",') }
-  its(:content) { should match(/mirror_type: "http",/) }
-  its(:content) { should match(/signature_type: "none",/) }
-  its(:content) { should match(/fingerprints: "",/) }
-  its(:content) { should match(/enabled: yes/) }
+  its(:content_as_yaml) do
+    should include(
+      "Reallyenglish" => include(
+        "url" => "http://10.3.build.reallyenglish.com/${ABI}",
+        "mirror_type" => "http",
+        "signature_type" => "none",
+        "fingerprints" => "NONE",
+        "enabled" => true
+      )
+    )
+  end
 end
